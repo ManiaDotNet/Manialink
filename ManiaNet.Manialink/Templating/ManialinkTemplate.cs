5d7f932c472dfcbbc722b4ae850f6862b80f5380
Arne Kiesewetter <banane1996@gmx.net>
2014-06-17 22:22:02 +1200

    Start of manialink templating...

---
+++ ManiaNet.Manialink/Templating/ManialinkTemplate.cs
@@ -0,0 +1,24 @@
+﻿using RazorEngine.Templating;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ManiaNet.Manialink.Templating
+{
+    /// <summary>
+    /// The base for manialink templates.
+    /// </summary>
+    /// <typeparam name="TModel">The type of the Model.</typeparam>
+    public class ManialinkTemplate<TModel> : TemplateBase<TModel>
+    {
+        /// <summary>
+        /// Gets or sets an object containing arbitrary information used by the view.
+        /// </summary>
+        public TModel Model { get; set; }
+
+        /// <summary>
+        /// Gets or sets a collection of styles used for different elements in the manialink.
+        /// </summary>
+        public StyleCollection Styles { get; set; }
+    }
+}
---
+++ ManiaNet.Manialink/Templating/StyleCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ManiaNet.Manialink.Templating
+{
+    /// <summary>
+    /// Represents information about the styles used for different elements in a manialink.
+    /// </summary>
+    public class StyleCollection
+    {
+        /// <summary>
+        /// Gets or sets the style for a message box quad.
+        /// </summary>
+        public string MessageBox { get; set; }
+
+        /// <summary>
+        /// Gets or sets the style for a Button in a message box.
+        /// </summary>
+        public string MessageBoxButton { get; set; }
+
+        // TODO: Add more; this is just an example.
+    }
+}
